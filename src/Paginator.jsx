8a421d2ad53dfe92ddb312eeab22ab4e6c62693c
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-10-14 14:49:02 +0100

    Don't render anything in Paginator if there's nothing to show

--- src/Paginator.jsx
+++ src/Paginator.jsx
@@ -9,6 +9,7 @@ var Link = Router.Link
 
 var Paginator = React.createClass({
   render: function() {
+    if (this.props.page == 1 && !this.props.hasNext) { return null }
     return <div className="Paginator">
       {this.props.page > 1 && <span className="Paginator__prev">
         <Link to={this.props.route} query={{page: this.props.page - 1}}>Prev</Link>
