ad109cd00805d2e3a289871cf1e03befe1954d4d
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-10-17 08:56:09 +0100

    Moved the site title to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   ITEMS_PER_PAGE: 30
+, SITE_TITLE: 'React Hacker News'
 , TOP_STORIES: 100
 , UPDATE_CACHE_SIZE: 200
 }
--- src/utils/setTitle.js
+++ src/utils/setTitle.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var SITE_TITLE = 'React Hacker News'
+var SITE_TITLE = require('./constants').SITE_TITLE
 
 function setTitle(title) {
   document.title = (title ? title + ' | ' + SITE_TITLE : SITE_TITLE)
