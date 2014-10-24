e307ad17097d0ca1126fd95dcd506f8b53e2131c
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-10-24 12:01:52 +0100

    [fixed] Spinner width calculation was appending strings

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -14,7 +14,7 @@ var Spinner = React.createClass({
   render: function() {
     var bounceSize = this.props.size + 'px'
     var bounceStyle = {height: bounceSize, width: bounceSize, marginRight: this.props.spacing + 'px'}
-    return <div className="Spinner" style={{width: ((this.props.size + this.props.spacing) * 3) + 'px'}}>
+    return <div className="Spinner" style={{width: ((Number(this.props.size) + Number(this.props.spacing)) * 3) + 'px'}}>
       <div className="bounce1" style={bounceStyle}/>
       <div className="bounce2" style={bounceStyle}/>
       <div className="bounce3" style={bounceStyle}/>
