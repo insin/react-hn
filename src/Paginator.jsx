89f455a37d587441831e4e1a3afa467d15b06e78
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-03-03 13:51:48 +0000

    Scroll to the top when paginator links are clicked
    (After letting react-router grab the current position for history navigation)
    
    react-router doesn't automatically scroll to the top if only a query param has changed

--- src/Paginator.jsx
+++ src/Paginator.jsx
@@ -4,15 +4,19 @@ var React = require('react')
 var {Link} = require('react-router')
 
 var Paginator = React.createClass({
+  _onClick(e) {
+    setTimeout(function() { window.scrollTo(0, 0) }, 0)
+  },
+
   render() {
     if (this.props.page == 1 && !this.props.hasNext) { return null }
     return <div className="Paginator">
       {this.props.page > 1 && <span className="Paginator__prev">
-        <Link to={this.props.route} query={{page: this.props.page - 1}}>Prev</Link>
+        <Link to={this.props.route} query={{page: this.props.page - 1}} onClick={this._onClick}>Prev</Link>
       </span>}
       {this.props.page > 1 && this.props.hasNext && ' | '}
       {this.props.hasNext && <span className="Paginator__next">
-        <Link to={this.props.route} query={{page: this.props.page + 1}}>More</Link>
+        <Link to={this.props.route} query={{page: this.props.page + 1}} onClick={this._onClick}>More</Link>
       </span>}
     </div>
   }
