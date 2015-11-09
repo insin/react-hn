c7f6dc7ad9b80e99a6b9882742ef53227ee9b9a0
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-11-09 23:50:14 +0000

    Restore old scrolling behaviour using scroll-behavior

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ var createHashHistory = require('history/lib/createHashHistory')
 var React = require('react')
 var Router = require('react-router/lib/Router')
 var {render} = require('react-dom')
+var useScroll = require('scroll-behavior/lib/useStandardScroll')
 
 var routes = require('./routes')
 
-// Opt out of persistent state query key for for hash history
-// See https://github.com/rackt/history/blob/master/docs/HashHistoryCaveats.md
-var history = createHashHistory({queryKey: false})
+var history = useScroll(createHashHistory)()
 
 render(<Router history={history} routes={routes}/>, document.getElementById('app'))
