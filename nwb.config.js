24223b541fa0011b007368d88f1707ee9cd49f2c
Addy Osmani <addyosmani@gmail.com>
2016-10-02 22:37:43 -0700

    Update nwb.config to latest loader, babel requirements

--- nwb.config.js
+++ nwb.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   type: 'react-app',
   babel: {
-    loose: 'all'
+    loose: 'all',
+    stage: false,
+    presets: ['es2015', 'stage-0', 'react']
   },
   webpack: {
+    loaders: {
+      babel: {
+        babelrc: true,
+        cacheDirectory: true
+      }
+    },
     plugins: {
       define: {
         __VERSION__: JSON.stringify(require('./package.json').version)
       }
-    }
+    }    
   }
 }
