52e93813b01c45aa48339e638c8fa1ab39b855ef
Addy Osmani <addyosmani@gmail.com>
2016-10-05 18:10:33 -0700

    Add HtmlWebpackPlugin for re-wiring up template output

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,6 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+// var ChunkManifestPlugin = require('chunk-manifest-webpack-plugin')
+
 module.exports = {
   type: 'react-app',
   babel: {
@@ -16,6 +19,20 @@ module.exports = {
       define: {
         __VERSION__: JSON.stringify(require('./package.json').version)
       }
+    },
+    extra: {
+      plugins: [
+        new HtmlWebpackPlugin({
+          filename: 'views/index.ejs',
+          template: 'src/views/index.ejs',
+          markup: '<%- markup %>'
+        }),
+        new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: 'public/index-static.html',
+          markup: '<%- markup %>'
+        })
+      ]
     }    
   }
 }
