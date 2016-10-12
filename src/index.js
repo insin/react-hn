083ac28ca3e3277db10a10e59c6e025652c1afc9
Addy Osmani <addyosmani@gmail.com>
2016-10-11 17:33:38 -0700

    Update routing to work with latest dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 require('setimmediate')
 
-var createHashHistory = require('history/lib/createHashHistory')
+// var createHashHistory = require('history/lib/createHashHistory')
 var React = require('react')
 var {render} = require('react-dom')
 var Router = require('react-router/lib/Router')
-var useRouterHistory = require('react-router/lib/useRouterHistory')
-var withScroll = require('scroll-behavior/lib/withStandardScroll')
+// var useRouterHistory = require('react-router/lib/useRouterHistory')
+// var withScroll = require('scroll-behavior/lib/withStandardScroll')
 
 var routes = require('./routes')
 
-var history = withScroll(useRouterHistory(createHashHistory)())
+// var history = withScroll(useRouterHistory(createHashHistory)())
 
-render(<Router history={history} routes={routes}/>, document.getElementById('app'))
+render(<Router routes={routes}/>, document.getElementById('app'))
