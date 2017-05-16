60dc0b7a7190a79a77d48b79d12d90568b37be93
Jeff Posnick <jeffy@google.com>
2017-05-15 18:55:03 -0700

    Moves over to the Workbox libraries.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,68 +1,11 @@
-importScripts('sw-lib.v0.0.16.min.js');
-importScripts('precache-manifest.js');
+importScripts('https://unpkg.com/workbox-sw@0.0.2');
 
-self.goog.swlib.cacheRevisionedAssets(self.__file_manifest);
+const workboxSW = new WorkboxSW({clientsClaim: true});
 
-// Runtime caching
-self.goog.swlib.router.registerRoute(
-  new RegExp('/(.*)'),
-  self.goog.swlib.staleWhileRevalidate({
-    cacheName: 'runtime-caching'
-  })
-);
+// This array will be populated by workboxBuild.injectManifest() when the
+// production service worker is generated.
+workboxSW.precache([]);
 
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-
-// Servers with Google resources needed
-// self.goog.swlib.router.registerRoute(
-//   new RegExp('^https://googleapis.com'),
-//   self.goog.swlib.staleWhileRevalidate({
-//     cacheName: 'google-apis'
-//   })
-// );
-
-// self.goog.swlib.router.registerRoute(
-//   new RegExp('^https://gstatic.com'),
-//   self.goog.swlib.staleWhileRevalidate({
-//     cacheName: 'g-static'
-//   })
-// );
-
-// self.goog.swlib.router.registerRoute(
-//   new RegExp('^https://firebaseio.com'),
-//   self.goog.swlib.staleWhileRevalidate({
-//     cacheName: 'firebase-io'
-//   })
-// );
-
-// self.goog.swlib.router.registerRoute(
-//   new RegExp('^https://appspot.com'),
-//   self.goog.swlib.staleWhileRevalidate({
-//     cacheName: 'app-spot'
-//   })
-// );
-
-// Hacker News data sources
-// Including fallbacks (third-party mirrors)
-
-// self.goog.swlib.router.registerRoute(
-//   new RegExp('^https://hacker-news.firebaseio.com'),
-//   self.goog.swlib.staleWhileRevalidate({
-//     cacheName: 'hacker-news'
-//   })
-// );
-
-// self.goog.swlib.router.registerRoute(
-//   new RegExp('^https://s-usc1c-nss-136.firebaseio.com'),
-//   self.goog.swlib.staleWhileRevalidate({
-//     cacheName: 'hacker-news'
-//   })
-// );
-
-// self.goog.swlib.router.registerRoute(
-//   new RegExp('^https://node-hnapi.herokuapp.com'),
-//   self.goog.swlib.staleWhileRevalidate({
-//     cacheName: 'node-hnapi'
-//   })
-// );
+workboxSW.router.setDefaultHandler({
+  handler: workboxSW.strategies.staleWhileRevalidate()
+});
