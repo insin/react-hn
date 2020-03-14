1b73bf1b5297b1de3f844d9b7d86cdf65c3d0e05
Jonny Buchanan <jonathan.buchanan@gmail.com>
2020-03-15 01:07:44 +1000

    Release v1.7.0

--- nwb.config.js
+++ nwb.config.js
@@ -3,6 +3,8 @@ module.exports = {
   webpack: {
     define: {
       __VERSION__: JSON.stringify(require('./package.json').version)
-    }
+    },
+    // Path-independent build which doesn't have to be served at /
+    publicPath: ''
   }
 }
